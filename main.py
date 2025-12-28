933a9d6ba1ed0b529654d90dcb12b3208de176b0
rohxama <rohxama@gmail.com>
2025-12-28 14:24:21 +0500

    Datatypes done

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ print("hello World")
 rohama = 23
 print(rohama) 
 
+# Datatypes wo bhi sirf 3 (int, str, bool) ✅
+a = 1 # interger wrote as int
+b = "Rohama" # string wrote as str
+c = 0.5 # float 
+d = 3/2 # complex
+e = True # Boolean wrote as bool 
+f = False # Boolean wrote as bool 
+g = 'Salfi' # string 
+
+# Checking the type of varaible ✅
+print(type(a)) 
