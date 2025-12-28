256ddbaaf60887a7dc96c0666eff318d7536e69d
rohxama <rohxama@gmail.com>
2025-12-28 14:00:18 +0500

    Making variable done

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # First ever python code jo har koi likhta hai mn nay bhi likh lye ✅
 print("hello World")
+
+# Making a variable or bhayia itna asaan kayyy mazaa aagaya ✅
+rohama = 23
+print(rohama) 
+
