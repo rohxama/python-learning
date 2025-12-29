aace629b1124967a2eb55e7dc23187c7c4805d31
rohxama <rohxama@gmail.com>
2025-12-29 12:06:31 +0500

    Type casting with truthy nad falsy values done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# First ever python code jo har koi likhta hai mn nay bhi likh lye ✅
+## First ever python code jo har koi likhta hai mn nay bhi likh lye ✅
 print("hello World")
 
-# Making a variable or bhayia itna asaan kayyy mazaa aagaya ✅
+## Making a variable or bhayia itna asaan kayyy mazaa aagaya ✅
 rohama = 23
 print(rohama) 
 
-# Datatypes wo bhi sirf 3 (int, str, bool) ✅
+## Datatypes wo bhi sirf 3 (int, str, bool) jo main hain ✅
 a = 1 # interger wrote as int
 b = "Rohama" # string wrote as str
 c = 0.5 # float 
@@ -14,5 +14,14 @@ e = True # Boolean wrote as bool
 f = False # Boolean wrote as bool 
 g = 'Salfi' # string 
 
-# Checking the type of varaible ✅
+## Checking the type of varaible ✅
 print(type(a)) 
+
+## Type casting ✅
+age = "28"
+print(age, type(age))
+
+age = int(age)
+print(age, type(age))  # Same as str(value), bool(value), float(bool) and so on....
+
+# Ab agr boolean ke bt kee jaaye kay issko kse krna hai toh iss mein aa jati hain 2 values truthy and falsy, ab yahn asay maan lo kay sb data types, sb values truthy hoti hain means true, and kuch hee values falsy hoti hain jase kay (0, none, "", undefined, False). Ab agr variables mein falsy values ayyein toh samaj lena ye false hain or baqi sb values true.
